c948bcd7bd02bd7b6cd751a81a9e6dbf23462f6c
IsaSofi <isasofi@unicauca.edu.co>
2023-06-12 11:55:45 -0500

    "Actualización registro"

--- app/src/main/java/edu/unicauca/aplimovil/unicar/ui/Registro.kt
+++ app/src/main/java/edu/unicauca/aplimovil/unicar/ui/Registro.kt
@@ -7,31 +7,45 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Checkbox
+import androidx.compose.material.RadioButton
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
@@ -54,6 +68,10 @@ fun registroScreen(navController : NavHostController,modifier: Modifier = Modifi
     val celularValue = remember { mutableStateOf(TextFieldValue())}
     val isSaved = remember { mutableStateOf(false) }
     val showError = remember { mutableStateOf(false) }
+    val focusRequester = remember { FocusRequester() }
+    val focusManager = LocalFocusManager.current
+    var selectedOption by remember { mutableStateOf("Pasajero") }
+
     Column(
         modifier = Modifier
             .verticalScroll(scrollState)
@@ -75,7 +93,8 @@ fun registroScreen(navController : NavHostController,modifier: Modifier = Modifi
             Image(
                 painter = painterResource(R.drawable.unicar_sinfondo),
                 contentDescription = "1",
-                modifier = Modifier.fillMaxSize()
+                modifier = Modifier
+                    .fillMaxSize()
                     .scale(0.8f)
             )
         }
@@ -141,10 +160,19 @@ fun registroScreen(navController : NavHostController,modifier: Modifier = Modifi
                         .height(50.dp)
                         .fillMaxWidth()
                         .clip(shape = RoundedCornerShape(15.dp))
+                        .focusRequester(focusRequester)
                         .background(colorResource(id = R.color.azul2)),
                     value = nombreValue.value,
                     onValueChange = { nombreValue.value = it },
-                    label = { Text("Ingresa tu nombre") }
+                    label = { Text("Ingresa tu nombre") },
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        imeAction = ImeAction.Next
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onNext = {
+                            focusManager.moveFocus(FocusDirection.Down)
+                        }
+                    )
                 )
                 Spacer(modifier = Modifier.height(10.dp))
                 Text(text = "Identificación",
@@ -158,10 +186,20 @@ fun registroScreen(navController : NavHostController,modifier: Modifier = Modifi
                         .height(50.dp)
                         .fillMaxWidth()
                         .clip(shape = RoundedCornerShape(15.dp))
+                        .focusRequester(focusRequester)
                         .background(colorResource(id = R.color.azul2)),
                     value = id_usuarioValue.value,
                     onValueChange = { id_usuarioValue.value = it },
-                    label = { Text("Ingresa tu identificación") }
+                    label = { Text("Ingresa tu identificación") },
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Number,
+                        imeAction = ImeAction.Next
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onNext = {
+                            focusManager.moveFocus(FocusDirection.Down)
+                        }
+                    )
                 )
                 Spacer(modifier = Modifier.height(10.dp))
                 Text(text = "Correo Institucional",
@@ -175,10 +213,19 @@ fun registroScreen(navController : NavHostController,modifier: Modifier = Modifi
                         .height(50.dp)
                         .fillMaxWidth()
                         .clip(shape = RoundedCornerShape(15.dp))
+                        .focusRequester(focusRequester)
                         .background(colorResource(id = R.color.azul2)),
                     value = correoValue.value,
                     onValueChange = { correoValue.value = it },
-                    label = { Text("Ingresa tu correo institucional") }
+                    label = { Text("Ingresa tu correo institucional") },
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        imeAction = ImeAction.Next
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onNext = {
+                            focusManager.moveFocus(FocusDirection.Down)
+                        }
+                    )
                 )
                 Spacer(modifier = Modifier.height(10.dp))
                 Text(text = "Contraseña",
@@ -192,11 +239,21 @@ fun registroScreen(navController : NavHostController,modifier: Modifier = Modifi
                         .height(50.dp)
                         .fillMaxWidth()
                         .clip(shape = RoundedCornerShape(15.dp))
-                        .background(colorResource(id = R.color.azul2)),
+                        .focusRequester(focusRequester)
+                        .background(colorResource(id = R.color.azul2))
+                        .focusRequester(focusRequester),
                     value = claveValue.value,
                     onValueChange = { claveValue.value = it },
                     label = { Text("Ingresa tu contraseña") },
-                    visualTransformation = PasswordVisualTransformation()
+                    visualTransformation = PasswordVisualTransformation(),
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        imeAction = ImeAction.Next
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onNext = {
+                            focusManager.moveFocus(FocusDirection.Down)
+                        }
+                    )
 
                 )
                 Spacer(modifier = Modifier.height(10.dp))
@@ -211,11 +268,20 @@ fun registroScreen(navController : NavHostController,modifier: Modifier = Modifi
                         .height(50.dp)
                         .fillMaxWidth()
                         .clip(shape = RoundedCornerShape(15.dp))
+                        .focusRequester(focusRequester)
                         .background(colorResource(id = R.color.azul2)),
                     value = confirmarClaveValue.value,
                     onValueChange = { confirmarClaveValue.value = it },
                     label = { Text("Ingresa nuevamente tu contraseña") },
-                    visualTransformation = PasswordVisualTransformation()
+                    visualTransformation = PasswordVisualTransformation(),
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        imeAction = ImeAction.Next
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onNext = {
+                            focusManager.moveFocus(FocusDirection.Down)
+                        }
+                    )
 
                 )
                 Spacer(modifier = Modifier.height(10.dp))
@@ -233,8 +299,54 @@ fun registroScreen(navController : NavHostController,modifier: Modifier = Modifi
                         .background(colorResource(id = R.color.azul2)),
                     value = celularValue.value,
                     onValueChange = { celularValue.value = it },
-                    label = { Text("Ingresa tu número de teléfono") }
+                    label = { Text("Ingresa tu número de teléfono") },
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Number,
+                        imeAction = ImeAction.Done
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onDone = {focusManager.clearFocus()}
+                    )
                 )
+                Spacer(modifier = Modifier.height(10.dp))
+                Row(modifier = Modifier
+                    .fillMaxSize()
+                ) {
+                    Text(
+                        text = "Rol: ",
+                        style = TextStyle(fontSize = 16.sp,
+                            color = colorResource(id = R.color.azul1),
+                            fontWeight = FontWeight.Bold)
+                    )
+                    Spacer(modifier = Modifier.height(5.dp))
+                    val roles = listOf("pasajero")
+                    var selectedRole by remember { mutableStateOf("pasajero") } // Opción "pasajero" seleccionada por defecto
+
+                    Column (verticalArrangement = Arrangement.Center ) {
+                        roles.forEach { role ->
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                                modifier = Modifier
+                                    .padding(vertical = 8.dp)
+                                    .selectable(
+                                        selected = (selectedRole == role),
+                                        onClick = { selectedRole = role }
+                                    )
+                                    .padding(horizontal = 16.dp)
+                            ) {
+                                RadioButton(
+                                    selected = (selectedRole == role),
+                                    onClick = { selectedRole = role }
+                                )
+                                Text(
+                                    text = role,
+                                    style = TextStyle(fontSize = 16.sp)
+                                )
+                            }
+                        }
+                    }
+                }
+
                 Spacer(modifier = Modifier.height(10.dp))
                 Column(
                     modifier = Modifier
